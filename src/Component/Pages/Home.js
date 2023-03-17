b6e42409e6a7393be8db75889d334720538c1af9
rampion19 <princegupta.pg19@gmail.com>
2023-03-18 00:04:52 +0530

    Verify User Email

--- src/Component/Pages/Home.js
+++ src/Component/Pages/Home.js
@@ -3,17 +3,50 @@ import { NavLink } from "react-router-dom";
 
 import "./Home.css";
 const Home = () => {
-  return (
-    <div className="main-home">
-      <h2 className="margin-auto">Welcome To Expense Tracker</h2>
-      <div className="profile">
-        Your Profile is incomplete<br></br>
-        <NavLink className="link-profile" to="/update">
-          complete now
-        </NavLink>
-      </div>
-    </div>
-  );
+
+    const verifyEmail = async (event) => {
+        event.preventDefault();
+        try {
+            const res = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDZnqHwYDKWIazKAZlYNcW3CnY3LV1aWAU',
+                {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': "application/json"
+                    },
+                    body: JSON.stringify({
+                        requestType: "VERIFY_EMAIL",
+                        idToken: JSON.parse(localStorage.getItem("idToken")).idToken,
+                    })
+                })
+
+            if (res.ok) {
+                alert("Verification mail is sent, please check your mail")
+            }
+
+            const data = await res.json();
+            console.log(data);
+        }
+        catch (error) {
+            console.log(error.message)
+        }
+    }
+    return (
+        <>
+            <div className="main-home">
+                <h2 className="margin-auto">Welcome To Expense Tracker</h2>
+                <div className="profile">
+                    Your Profile is incomplete<br></br>
+                    <NavLink className="link-profile" to="/update">
+                        complete now
+                    </NavLink>
+                </div>
+            </div>
+            <div className='verify-email text-center pt-1' >
+                <button style={{ "cursor": "pointer", "color": "blue" }} onClick={verifyEmail}>
+                    Verify Email</button>
+            </div>
+        </>
+    );
 };
 
 export default Home;
