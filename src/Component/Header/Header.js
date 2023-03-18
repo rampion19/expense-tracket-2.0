012a376299ea9f26ca440fff5b22ab2144f0f44c
rampion19 <princegupta.pg19@gmail.com>
2023-03-18 13:10:56 +0530

    added a logout button and navigate it to login

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import "./Header.css";
-import {NavLink} from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 
- const Header = () => {
+const Header = () => {
+  const navigate = useNavigate();
+  const logoutHandler = async () => {
+
+    await localStorage.removeItem('idToken');
+    navigate('/login')
+    alert("logout Successful")
+  }
   return (
     <div className='mainNav'>
-    <nav>
+      <nav>
         <ul>
-            <li><NavLink to="/home">Home</NavLink> </li>
-             <li><NavLink to="/expenses">Expenses</NavLink></li>
-            <li><NavLink to="/about">About</NavLink></li>
-            <li><NavLink to="/profile">Profile</NavLink></li>
-            <li><NavLink to="/login">Login</NavLink></li> 
+          <li><NavLink to="/home">Home</NavLink> </li>
+          <li><NavLink to="/expenses">Expenses</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/profile">Profile</NavLink></li>
+          <li><NavLink to="/login">Login</NavLink></li>
+          <li><NavLink onClick={logoutHandler}>logout</NavLink></li>
         </ul>
-    </nav>
+      </nav>
     </div>
   )
 }
