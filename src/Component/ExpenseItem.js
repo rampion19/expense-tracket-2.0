73a4e3bba0a55c5dec09ae0cf9f673ed4e70f626
rampion19 <princegupta.pg19@gmail.com>
2023-03-19 21:19:59 +0530

    added edit and delete button

--- src/Component/ExpenseItem.js
+++ src/Component/ExpenseItem.js
@@ -1,15 +1,62 @@
-import React from 'react'
+import React from 'react';
+import './ExpenseItem.css';
+
+
 
 const ExpenseItem = (props) => {
-    return (
-        <div>
-            <li>
-                {`${props.item.amount}  
-            ${props.item.description}   
-            ${props.item.category}`}
 
-            </li>
-        </div>
+    const deleteHandler = async () => {
+        try {
+            const res = await fetch(`https://data-edd3c-default-rtdb.firebaseio.com/user-expenses/${props.item.id}.json`,
+                {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                })
+            const data = await res.json();
+
+            if (res.ok) {
+
+                alert("Expense Deleted Successfully")
+                props.deleteItem(props.item)
+
+            } else {
+                throw data.error
+            }
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    const editHandler = async () => {
+        try {
+            const res = await fetch(`https://data-edd3c-default-rtdb.firebaseio.com/user-expenses/${props.item.id}.json`,
+                {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                })
+            const data = await res.json();
+
+            if (res.ok) {
+                props.editItem(props.item)
+
+            } else {
+                throw data.error
+            }
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    return (<>
+        <li className='expenselist'> <b>Category</b> :-{props.item.category} <b>Amount</b>:-${props.item.amount} <b>Description</b>:-{props.item.description} <button className='bg-blue' onClick={editHandler}>Edit</button> <button className='bg-red' onClick={deleteHandler}>Delete</button> </li>
+        {/* <li><span>{props.item.amount}</span>  <span>{props.item.description}</span>  <span> {props.item.category}</span> `
+        <button onClick={editHandler}>Edit</button>   <button onClick={deleteHandler}>Delete</button> </li> */}
+    </>
     )
 }
-export default ExpenseItem;
+
+export default ExpenseItem
--- src/Component/Pages/Expenses.js
+++ src/Component/Pages/Expenses.js
@@ -1,110 +1,105 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./Expenses.css";
 import ExpenseItem from "../ExpenseItem";
-
 const Expenses = () => {
-    const inputAmountRef = useRef();
-    const inputDescRef = useRef();
-    const inputCategoryRef = useRef();
-    const dummy_expenses = [];
-    const [expenses, setExpense] = useState(dummy_expenses)
-
-    const fetchExpense = async () => {
-        try {
-            const res = await fetch(`https://expense-tracker-364f5-default-rtdb.firebaseio.com/Expenses.json`,
-                {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                }
-            );
-            const data = await res.json();
-            console.log(data);
-
-            if (res.ok) {
-                const newData = [];
-                for (let key in data) {
-                    newData.push({
-                        id: key,
-                        ...data[key]
-                    });
-                }
-                setExpense(newData)
-            } else {
-                throw data.error;
-            }
-        } catch (error) {
-            console.log(error.message)
+  const inputAmountRef = useRef();
+  const inputDescRef = useRef();
+  const inputCategoryRef = useRef();
+  const dummy_expenses = [];
+  const [expenses, setExpenses] = useState(dummy_expenses);
+  async function fetchExpenses() {
+    try {
+      const res = await fetch(
+        `https://expense-tracker-364f5-default-rtdb.firebaseio.com/user-expenses.json`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
+      );
+      const data = await res.json();
+      console.log(data);
+      if (res.ok) {
+        const newdata = [];
+        for (let key in data) {
+          newdata.push({ id: key, ...data[key] });
+        }
+        setExpenses(newdata);
+      } else {
+        throw data.error;
+      }
+    } catch (error) {
+      console.log(error.message);
     }
+  }
 
-    useEffect(() => {
-        fetchExpense();
-    }, [])
 
-    const addExpenseHandler = async (event) => {
-        event.preventDefault();
-
-        const obj = {
-            amount: inputAmountRef.current.value,
-            description: inputDescRef.current.value,
-            category: inputCategoryRef.current.value,
-        };
-        try {
-            const res = await fetch(`https://expense-tracker-364f5-default-rtdb.firebaseio.com/Expenses.json`,
-                {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(obj),
-                }
-            )
-            const data = await res.json();
-            if (res.ok) {
-                alert("expenses added successfully")
-                inputAmountRef.current.value = "";
-                inputDescRef.current.value = "";
-                inputCategoryRef.current.value = "";
-                await fetchExpense();
-            } else {
-                throw data.error;
-            }
-        } catch (error) {
-            console.log(error.message)
-        }
+  const addExpenseHandler =  async (event) => {
+    event.preventDefault();
+    const obj = {
+      amount: inputAmountRef.current.value,
+      description: inputDescRef.current.value,
+      category: inputCategoryRef.current.value,
     };
-    return (
-        <div>
-            <div>
-                <form onSubmit={addExpenseHandler} className="form-expenses">
-                    <label htmlFor="amount">Amount</label>
-                    <input ref={inputAmountRef} type="number" id="amount" />
-                    <label htmlFor="desc">Description</label>
-                    <textarea
-                        type="text"
-                        id="desc"
-                        rows="3"
-                        ref={inputDescRef}
-                    ></textarea>
-                    <label htmlFor="category">Choose a car:</label>
-                    <select ref={inputCategoryRef} id="category">
-                        <option value="Food">Food</option>
-                        <option value="Shopping">Shopping</option>
-                        <option value="Rent">Rent</option>
-                        <option value="Others">Others</option>
-                    </select>
-                    <button type="submit">Add Expense</button>
-                </form>
-            </div>
-            <div className="expenses-list">
-                {expenses.map((expense) => (
-                    <ExpenseItem key={expense.id} item={expense} />
-                ))}
-            </div>
-        </div>
-    );
-};
+    try {
+      const res = await fetch(
+        `https://expense-tracker-364f5-default-rtdb.firebaseio.com/user-expenses.json`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(obj),
+        }
+      );
+      const data = await res.json();
+      if (res.ok) {
+        alert("Expense added Successfully");
+        inputAmountRef.current.value = "";
+        inputDescRef.current.value = "";
+        inputCategoryRef.current.value = "";
+        await fetchExpenses();
+      } else {
+        throw data.error;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  useEffect(() => {
+    fetchExpenses();
+  },[]);
+  return (
+    <div>
+      <div>
+        <form onSubmit={addExpenseHandler} className="form-expenses">
+          <label htmlFor="amount">Amount</label>
+          <input ref={inputAmountRef} type="number" id="amount" />
+          <label htmlFor="desc">Description</label>
+          <textarea
+            type="text"
+            id="desc"
+            rows="3"
+            ref={inputDescRef}
+          ></textarea>
+          <label htmlFor="category">Choose a car:</label>
+          <select ref={inputCategoryRef} id="category">
+            <option value="Food">Food</option>
+            <option value="Shopping">Shopping</option>
+            <option value="Rent">Rent</option>
+            <option value="Others">Others</option>
+          </select>
+          <button type="submit">Add Expense</button>
+        </form>
+      </div>
+      <div className="expenses-list">
+        {expenses.map((expense) => (
+          <ExpenseItem key={expense.id} item={expense} />
+        ))}
+      </div>
+    </div>
+  );
+};
 export default Expenses;
